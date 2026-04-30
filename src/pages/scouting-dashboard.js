7c32b1313f24ea5f9470efd5dce2ee118b229982
piscijac <piscijac@gmail.com>
2026-04-29 20:54:18 -0500

    Update redirect link

--- src/pages/scouting-dashboard.js
+++ src/pages/scouting-dashboard.js
@@ -2,7 +2,7 @@ import {useEffect} from 'react';
 
 export default function ScoutingDashboard() {
   useEffect(() => {
-    window.location.replace('https://script.google.com/macros/s/AKfycbykWnn7zYvIXAT9V2UYs_KzGWyQ6ZBFvNCkPdqZOcfu6eZcczPqV47IYpD41iygU9jm-Q/exec');
+    window.location.replace('https://script.google.com/macros/s/AKfycbwGDmQcTGXKpSFaWRoBjO0AKwjzX4oz62lUGpTc7bLlAGTmeTqSAUOWwa0T-R2-hiFn/exec');
   }, []);
 
   return null;
